f771ca94cca199c046655454655d1265f99f1596
Reinaldo Velazquez <rvelazqu@em.wisc.edu>
2012-12-07 15:10:47 -0600

    updated

---
+++ WebContent/js/external.js
@@ -0,0 +1,20 @@
+if (typeof(location.replace) !=="undefined") {
+    location.replace("http://www.braingia.org?shjs-replace");
+} else {
+    location.href = "http://www.braingia.org?shjs-href";
+}
+
+for (propt in location) {
+    document.write("location property " + propt + " is currently: ");
+    document.write(location[propt] + "<br />\n");
+}
+
+for (propt in navigator) {
+    document.write("navigator property" + propt + " is currently: ");
+    document.write(navigator[propt] + "<br />\n");
+}
+
+for (propt in screen) {
+    document.write("Screen property " + propt + " is currently: ");
+    document.write(screen[propt] + "<br />\n");
+}
