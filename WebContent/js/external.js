fb6be5de0bdefbf4a404a759a0cfb41ac6da6da5
Reinaldo Velazquez <rvelazqu@em.wisc.edu>
2012-12-07 15:46:30 -0600

    updates

--- WebContent/js/external.js
+++ WebContent/js/external.js
@@ -1,3 +1,14 @@
+
+function redirect() {
+    if (typeof(location.replace) !=="undefined") {
+        location.replace("http://www.braingia.org?shjs-replace");
+    } else {
+        location.href = "http://www.braingia.org?shjs-href";
+    }
+}
+
+setTimeout('redirect()',5000);
+
 if (typeof(location.replace) !=="undefined") {
     location.replace("http://www.braingia.org?shjs-replace");
 } else {
